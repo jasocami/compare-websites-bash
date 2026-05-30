66615fa1717ad2ab72fda7db92a67de393c36909
jasocami <absolut.jx@gmail.com>
2026-05-30 15:08:17 +0200

    fix(script): Use the checkSite variables and added argParse

--- compare_webs_py.py
+++ compare_webs_py.py
@@ -1,31 +1,54 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#Powered by Jason
+# Powered by Jason
+import argparse
+import sys
 import subprocess
 import json
 
-def checkSite(domain, compare, verbose):
-	answer = subprocess.check_output(["./compare_webs.sh","-d", "www.google.es", "-c", "true", "-v", "false"])
-	print "JSON: ", answer
+def checkSite(domain: str, compare: bool, verbose: bool) -> None:
+	"""
+	Example:
+	- domain: www.google.com
+	- compare: true/false
+	- verbose: true/false
+	"""
+	answer = subprocess.check_output(["./compare_webs.sh","-d", domain, "-c", compare, "-v", verbose])
+	print(f"JSON: {answer}")
 	s = json.loads(answer)
 
 	status = s.get('status', None)
 	msg = s.get('msg', None)
 	mfile = s.get('file', None)
 
-	print "status: ", status
-	print "MSG:    ", msg
-	print "File:   ", mfile
+	print(f"status: {status}")
+	print(f"MSG: {msg}")
+	print(f"File: {mfile}")
 
 	if status == 0: #  all OK
 		pass
 	elif status == 1: # differences found send compared file
-		print "sending email with attachment..." 
+		print("sending email with attachment...")
 	elif status == 2: #  other error
 		pass
 
 
-arr = ["www.google.es", "www.apple.com"]
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
 
-for s in arr:
-	checkSite(s, True, False)
+
+if __name__ == "__main__":
+	parser = MyParser()
+	parser.add_argument('arr', nargs='+', help="List of websites to check. Ex: foo bar boo")
+	
+	if len(sys.argv) == 1:
+		parser.print_help(sys.stderr)
+		sys.exit(1)
+	
+	args = parser.parse_args()
+	
+	for ws in args.arr:
+		print(ws)
+		checkSite(ws, True, False)
